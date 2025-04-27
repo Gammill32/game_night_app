db7d013a53c1de610532b1bf75f03faf1b61efc8
Gammill32 <Gammill32@gmail.com>
2025-04-27 16:53:12 -0500

    changed rankings to ratings for games

--- app/models.py
+++ app/models.py
@@ -35,7 +35,7 @@ class Person(db.Model, UserMixin):
     players = relationship('Player', back_populates='person', cascade='all, delete-orphan')
     owned_games = relationship('OwnedBy', back_populates='person', cascade='all, delete-orphan')
     wishlist_items = db.relationship('Wishlist', back_populates='person', cascade='all, delete-orphan')
-    rankings = relationship('GameRankings', back_populates='person', cascade='all, delete-orphan')
+    ratings = relationship('GameRatings', back_populates='person', cascade='all, delete-orphan')
 
     @property
     def is_admin_or_owner(self):
@@ -58,7 +58,7 @@ class Game(db.Model):
     nominations = db.relationship('GameNominations', back_populates='game', cascade='all, delete-orphan')
     votes = db.relationship('GameVotes', back_populates='game', cascade='all, delete-orphan')
     wishlist_entries = db.relationship('Wishlist', back_populates='game', cascade='all, delete-orphan')
-    rankings = relationship('GameRankings', back_populates='game', cascade='all, delete-orphan')
+    ratings = relationship('GameRatings', back_populates='game', cascade='all, delete-orphan')
 
 class OwnedBy(db.Model):
     __tablename__ = 'ownedby'
@@ -69,15 +69,15 @@ class OwnedBy(db.Model):
     game = relationship('Game', back_populates='owners')
     person = relationship('Person', back_populates='owned_games')
 
-class GameRankings(db.Model):
-    __tablename__ = 'game_rankings'
+class GameRatings(db.Model):
+    __tablename__ = 'game_Ratings'
     id = db.Column(db.Integer, primary_key=True)
     game_id = db.Column(db.Integer, ForeignKey('games.id'), nullable=False)
     person_id = db.Column(db.Integer, ForeignKey('people.id'), nullable=False)
     ranking = db.Column(db.Integer)
 
-    game = relationship('Game', back_populates='rankings')
-    person = relationship('Person', back_populates='rankings')
+    game = relationship('Game', back_populates='ratings')
+    person = relationship('Person', back_populates='ratings')
 
 class Player(db.Model):
     __tablename__ = 'players'
